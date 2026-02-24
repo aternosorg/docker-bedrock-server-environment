8e14fae6fa494d7d8d024ab2d12dcf7181c9fd2f
Matthias Neid <matthias@aternos.org>
2026-02-24 13:16:40 +0100

    add log file env var

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ docker run -v /path/to/server:/server -w /server aternos/bedrock-server-environm
 The image contains an entrypoint that automatically handles ARM emulation and loads old libraries for older Bedrock server versions.
 By default, the entrypoint expects the server executable to be named `bedrock_server` located in the working directory
 of the container. You can change this by setting the `BEDROCK_EXECUTABLE` environment variable to the path of your server executable.
+
+The default entrypoint also writes the server output to a file named `server.log` in the working directory. You can change this by 
+setting the `LOG_FILE` environment variable to the desired log file path.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SERVER_BIN="${BEDROCK_EXECUTABLE:-./bedrock_server}"
+LOG_OUTPUT="${LOG_FILE:-server.log}"
 
 if [ ! -f "$SERVER_BIN" ]; then
     echo "ERROR: Executable '$SERVER_BIN' not found in $PWD!"
@@ -31,9 +32,9 @@ if [ "$NEEDS_LEGACY_1_0" = true ]; then
     if [ "$IS_ARM" = true ]; then
         export BOX64_LD_LIBRARY_PATH=".:/usr/lib/legacy-x86_64"
         export BOX64_EMULATED_LIBS="libstdc++.so.6:libgcc_s.so.1:libssl.so.1.0.0:libcrypto.so.1.0.0"
-        box64 "$SERVER_BIN" 2>&1 | tee -a server.log
+        box64 "$SERVER_BIN" 2>&1 | tee -a "$LOG_OUTPUT"
     else
-        LD_LIBRARY_PATH=".:/usr/lib/legacy-x86_64:$LD_LIBRARY_PATH" "$SERVER_BIN" 2>&1 | tee -a server.log
+        LD_LIBRARY_PATH=".:/usr/lib/legacy-x86_64:$LD_LIBRARY_PATH" "$SERVER_BIN" 2>&1 | tee -a "$LOG_OUTPUT"
     fi
 
 elif [ "$NEEDS_LEGACY_1_1" = true ]; then
@@ -41,16 +42,16 @@ elif [ "$NEEDS_LEGACY_1_1" = true ]; then
     if [ "$IS_ARM" = true ]; then
         export BOX64_LD_LIBRARY_PATH=".:/usr/lib/legacy-x86_64"
         export BOX64_EMULATED_LIBS="libssl.so.1.1:libcrypto.so.1.1"
-        box64 "$SERVER_BIN" 2>&1 | tee -a server.log
+        box64 "$SERVER_BIN" 2>&1 | tee -a "$LOG_OUTPUT"
     else
-        LD_LIBRARY_PATH=".:/usr/lib/legacy-x86_64:$LD_LIBRARY_PATH" "$SERVER_BIN" 2>&1 | tee -a server.log
+        LD_LIBRARY_PATH=".:/usr/lib/legacy-x86_64:$LD_LIBRARY_PATH" "$SERVER_BIN" 2>&1 | tee -a "$LOG_OUTPUT"
     fi
 
 else
     if [ "$IS_ARM" = true ]; then
         export BOX64_LD_LIBRARY_PATH="."
-        box64 "$SERVER_BIN" 2>&1 | tee -a server.log
+        box64 "$SERVER_BIN" 2>&1 | tee -a "$LOG_OUTPUT"
     else
-        LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH" "$SERVER_BIN" 2>&1 | tee -a server.log
+        LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH" "$SERVER_BIN" 2>&1 | tee -a "$LOG_OUTPUT"
     fi
 fi
